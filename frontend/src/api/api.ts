40f325a4d8c8e41835ea67c29714ba8de8e0494b
Daarkosss <karolpilat59@gmail.com>
2024-05-15 14:22:42 +0200

    Correct indentations

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -11,52 +11,52 @@ export interface AppFile {
 
 class API {
 
-    async fetch<T>(
-        method: Method,
-        path: string,
-        body?: FormData | { [key: string]: unknown },
-        headers: HeadersInit = {},
-        isFormData: boolean = false
-    ): Promise<T> {
-        const options = {
-            method,
-            headers: isFormData ? {} : {
-                'Content-Type': 'application/json',
-                ...headers
-            },
-            body: isFormData ? body : JSON.stringify(body)
-        } as RequestInit;
+  async fetch<T>(
+    method: Method,
+    path: string,
+    body?: FormData | { [key: string]: unknown },
+    headers: HeadersInit = {},
+    isFormData: boolean = false
+  ): Promise<T> {
+    const options = {
+      method,
+      headers: isFormData ? {} : {
+        'Content-Type': 'application/json',
+        ...headers
+      },
+      body: isFormData ? body : JSON.stringify(body)
+    } as RequestInit;
 
-        const response = await fetch(`${PATH_PREFIX}${path}`, options);
-        const data = await response.json();
-        if (!response.ok) {
-            throw new Error(data.errorMessage || 'Wrong server response!');
-        }
-        return data;
+    const response = await fetch(`${PATH_PREFIX}${path}`, options);
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.errorMessage || 'Wrong server response!');
     }
+    return data;
+  }
 
-    async uploadFile(file: File, fileName: string): Promise<AppFile> {
-        const formData = new FormData();
-        formData.append('file', file);
-        formData.append('fileName', fileName);
-        return this.fetch<AppFile>('POST', 'files/upload', formData, {}, true);
-      }
-    
-      async deleteFile(fileId: number): Promise<void> {
-        return this.fetch<void>('DELETE', `files/${fileId}`);
-      }
-    
-      async updateFileName(fileId: number, fileName: string): Promise<AppFile> {
-        return this.fetch<AppFile>('PUT', `files/update/${fileId}`, { fileName });
-      }
-    
-      async downloadFile(fileId: number): Promise<void> {
-        window.location.href = `${PATH_PREFIX}files/download?fileId=${fileId}`;
-      }
-    
-      async fetchFiles(): Promise<AppFile[]> {
-        return this.fetch<AppFile[]>('GET', 'files/all');
-      }
+  async uploadFile(file: File, fileName: string): Promise<AppFile> {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('fileName', fileName);
+    return this.fetch<AppFile>('POST', 'files/upload', formData, {}, true);
+  }
+  
+  async deleteFile(fileId: number): Promise<void> {
+    return this.fetch<void>('DELETE', `files/${fileId}`);
+  }
+  
+  async updateFileName(fileId: number, fileName: string): Promise<AppFile> {
+    return this.fetch<AppFile>('PUT', `files/update/${fileId}`, { fileName });
+  }
+  
+  async downloadFile(fileId: number): Promise<void> {
+    window.location.href = `${PATH_PREFIX}files/download?fileId=${fileId}`;
+  }
+  
+  async fetchFiles(): Promise<AppFile[]> {
+    return this.fetch<AppFile[]>('GET', 'files/all');
+  }
 }
 
 export const api = new API();
