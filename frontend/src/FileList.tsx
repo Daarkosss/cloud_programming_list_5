d0438d36e4a4abb891dc03b171e9afe1c4cf3e4b
Daarkosss <karolpilat59@gmail.com>
2024-05-15 14:59:40 +0200

    Change file input button style

--- frontend/src/FileList.tsx
+++ frontend/src/FileList.tsx
@@ -1,18 +1,21 @@
 import { observer } from 'mobx-react-lite';
 import { store } from './store';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faDownload, faEdit, faSave } from '@fortawesome/free-solid-svg-icons';
+import { faTrash, faDownload, faEdit, faSave, faUpload } from '@fortawesome/free-solid-svg-icons';
 import './styles/FileList.scss';
 
 const FileList = observer(() => {
   const [fileInput, setFileInput] = useState<File | null>(null);
   const [editFileId, setEditFileId] = useState<number | null>(null);
   const [newFileName, setNewFileName] = useState<string>('');
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [selectedFileName, setSelectedFileName] = useState<string>('');
 
   const handleFileInput = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setFileInput(e.target.files[0]);
+      setSelectedFileName(e.target.files[0].name);
     }
   };
 
@@ -22,6 +25,7 @@ const FileList = observer(() => {
       return;
     }
     store.uploadFile(fileInput, fileInput.name);
+    setSelectedFileName(''); // Clear the file name after upload
   };
 
   const startEditing = (fileId: number, currentName: string) => {
@@ -38,8 +42,26 @@ const FileList = observer(() => {
   return (
     <div>
       <h1>Files Upload to S3</h1>
-      <input type="file" onChange={handleFileInput} />
-      <button onClick={uploadFile}>Upload</button>
+      <div className="file-input-container">
+        <input
+          type="file"
+          className="file-input"
+          ref={fileInputRef}
+          onChange={handleFileInput}
+        />
+
+        <button
+          className="action-button"
+          onClick={() => fileInputRef.current && fileInputRef.current.click()}
+        >
+          <FontAwesomeIcon icon={faUpload} /> Select File
+        </button>
+
+        {selectedFileName && <span className="file-name">{selectedFileName}</span>}
+      </div>
+      <button className="button-upload" onClick={uploadFile}>
+        Upload
+      </button>
       <div className='files-container'>
         <table>
           <thead>
